ad4a817ccd3937985232c4d7836f62393915a685
semper-idem <awojcik010@gmail.com>
2023-06-09 22:45:11 +0200

    Upadted to 1.20 removed config and dependencies

--- src/main/java/net/semperidem/nnvf/mixin/MixinGameRenderer.java
+++ src/main/java/net/semperidem/nnvf/mixin/MixinGameRenderer.java
@@ -3,12 +3,7 @@ package net.semperidem.nnvf.mixin;
 import net.minecraft.client.render.GameRenderer;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.effect.StatusEffects;
-import net.minecraft.util.math.MathHelper;
-import net.semperidem.nnvf.NNVFClient;
-import net.semperidem.nnvf.NNVFConfig;
-import net.semperidem.nnvf.NNVFHelper;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
@@ -21,15 +16,7 @@ import java.util.Optional;
 public abstract class MixinGameRenderer {
     @Inject(method = "getNightVisionStrength", at = @At("TAIL"), cancellable = true)
     private static void injectGetNightVisionStrength(LivingEntity entity, float tickDelta, CallbackInfoReturnable<Float> cir) {
-        float nvDuration = Optional.of(
-            Objects.requireNonNull(entity
-                    .getStatusEffect(StatusEffects.NIGHT_VISION))
-                    .getDuration())
-                    .orElse(0);
-        cir.setReturnValue(
-                nvDuration > NNVFClient.config.FADE_OUT_START_TICK ?
-                        1 : NNVFHelper.getNightVisionStrength(nvDuration, tickDelta)
-        );
+        float nightVisionDuration =  Optional.of(Objects.requireNonNull(entity.getStatusEffect(StatusEffects.NIGHT_VISION)).getDuration()).orElse(0);;
+        cir.setReturnValue(nightVisionDuration > 100 ? 1 : nightVisionDuration / 100);
     }
-
 }
