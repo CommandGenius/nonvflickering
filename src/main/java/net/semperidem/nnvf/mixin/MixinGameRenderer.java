c98701508259347ad8cb8d6bab27d100a8f39b54
CommandGenius <rvlcariati@gmail.com>
2024-07-09 13:10:05 -0400

    update to 1.20.6 and 1.21

--- src/main/java/net/semperidem/nnvf/mixin/MixinGameRenderer.java
+++ src/main/java/net/semperidem/nnvf/mixin/MixinGameRenderer.java
@@ -3,7 +3,6 @@ package net.semperidem.nnvf.mixin;
 import net.minecraft.client.render.GameRenderer;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.effect.StatusEffectInstance;
-import net.minecraft.entity.effect.StatusEffects;
 import net.minecraft.util.math.MathHelper;
 import net.semperidem.nnvf.NNVFClient;
 import org.spongepowered.asm.mixin.Mixin;
@@ -12,13 +11,15 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
+import static net.minecraft.entity.effect.StatusEffects.NIGHT_VISION;
+
 
 @Mixin(GameRenderer.class)
 public abstract class MixinGameRenderer {
 
     @Inject(method = "getNightVisionStrength", at = @At("TAIL"), cancellable = true)
     private static void injectGetNightVisionStrength(LivingEntity entity, float tickDelta, CallbackInfoReturnable<Float> cir) {
-        StatusEffectInstance se = entity.getStatusEffect(StatusEffects.NIGHT_VISION);
+        StatusEffectInstance se = entity.getStatusEffect(NIGHT_VISION);
         if (se == null) {
             return;
         }
